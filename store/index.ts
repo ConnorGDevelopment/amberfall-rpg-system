60fad8ce7bb575e6083dbbf504d0961d10c44386
Connor Guarino <connorgdevelopment@gmail.com>
2022-05-17 10:56:01 -0400

    Update index.ts

--- store/index.ts
+++ store/index.ts
@@ -1,14 +1,14 @@
-import {Store} from 'vuex'
+import Vuex from 'vuex'
 import { initialiseStores } from '~/utils/store-accessor'
 import Chars from '~/store/char-store'
 import Skills from '~/store/skill-store'
 
-const initializer = (store: Store<any>) => initialiseStores(store)
+const initializer = (store: any) => initialiseStores(store)
 export const plugins = [initializer]
 export * from '~/utils/store-accessor'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const store = new Store({
+const store = new Vuex.Store({
   modules: {
     chars: Chars,
     skills: Skills
